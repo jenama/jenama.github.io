bc210630482b512b1d235f3d62b5e9a80d6a49b1
jenama <johanneenama@gmail.com>
2021-02-08 17:21:11 -0500

    Attempting to add meta tags

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 import Footer from "./Components/Footer";
+import { Helmet } from "react-helmet";
 import "./App.css";
 
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
-        <Navbar/> 
-        <Homepage2/> 
-        <About/>
-        <Skills/> 
-        <Projects/>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <Navbar />
+        <Homepage2 />
+        <About />
+        <Skills />
+        <Projects />
         {/* <Contact/> */}
-        <Footer/>
+        <Footer />
       </div>
     );
   }
